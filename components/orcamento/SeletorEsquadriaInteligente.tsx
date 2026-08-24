6a87df9c4c2d3ccd980347f1f1c6a733ec961dfe
francisbirolim-beep <francisbirolim@gmail.com>
2026-08-23 23:31:44 -0300

    chore: marcar seletor visual como entrada oficial do orçamento

--- components/orcamento/SeletorEsquadriaInteligente.tsx
+++ components/orcamento/SeletorEsquadriaInteligente.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// Entrada estável do Orçamento: a implementação visual fica isolada em V2 sem quebrar os imports existentes.
 export { default } from './SeletorEsquadriaInteligenteV2'
 export type {
   SelecaoEsquadriaOrcamento,
