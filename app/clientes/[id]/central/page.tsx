d919774afdf8e71f73cf50a494d796b59f9a31e0
francisbirolim-beep <francisbirolim@gmail.com>
2026-08-26 15:59:20 -0300

    feat: adicionar rota da Central do Cliente

---
+++ app/clientes/[id]/central/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import Cliente360Dashboard from '@/components/clientes/Cliente360Dashboard'
+
+export default function CentralClientePage() {
+  const params = useParams()
+  const id = params?.id as string
+  if (!id) return null
+  return <Cliente360Dashboard clienteId={id} />
+}
